d2cd16d6203e8b530339692a111aeff7d4c647d4
Mownika25 <41233359+Mownika25@users.noreply.github.com>
2019-05-17 09:56:26 +0530

    commit after professor is added next

--- app/src/main/java/com/example/android/eduease/STUDENT.java
+++ app/src/main/java/com/example/android/eduease/STUDENT.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class STUDENT extends AppCompatActivity {
 
-
+    CardView sigin,log;
     TextView login;
     TextView signin;
 
@@ -20,6 +21,8 @@ public class STUDENT extends AppCompatActivity {
         setContentView(R.layout.activity_student);
         login = findViewById(R.id.login);
 
+        sigin= findViewById(R.id.signinCard);
+        log=findViewById(R.id.logCard);
         signin = findViewById(R.id.signin);
 
         signin.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +33,24 @@ public class STUDENT extends AppCompatActivity {
                 startActivity(t);
             }
         });
+        sigin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent t=new Intent(STUDENT.this,SignUpActivity.class) ;
+                startActivity(t);
+            }
+        });
+
         login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent t=new Intent(STUDENT.this,LogInActivity.class);
+                startActivity(t);
+            }
+        });
+        log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
